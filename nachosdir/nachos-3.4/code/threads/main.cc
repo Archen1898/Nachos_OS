78172633a4780a2edf4a6abf234775797a21ccd6
Archen <132870045+Archen1898@users.noreply.github.com>
2023-09-28 20:14:14 -0400

    Partial Success with first part of Exercise 1

--- nachosdir/nachos-3.4/code/threads/main.cc
+++ nachosdir/nachos-3.4/code/threads/main.cc
@@ -59,7 +59,7 @@ extern int testnum;
 
 // External functions used by this file
 
-extern void ThreadTest(void), Copy(char *unixFile, char *nachosFile);
+extern void ThreadTest(int), Copy(char *unixFile, char *nachosFile);
 extern void Print(char *file), PerformanceTest(void);
 extern void StartProcess(char *file), ConsoleTest(char *in, char *out);
 extern void MailTest(int networkID);
