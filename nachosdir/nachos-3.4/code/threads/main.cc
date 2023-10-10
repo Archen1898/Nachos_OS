1a1dea64ce84fa272ec03e546856fa8a5a0a7162
Anne Nguyen <anguy031@fiu.edu>
2023-10-10 05:59:35 -0400

    Exercise 4: Elevator (Correct main.cc)

--- nachosdir/nachos-3.4/code/threads/main.cc
+++ nachosdir/nachos-3.4/code/threads/main.cc
@@ -1,4 +1,4 @@
-// main.cc 
+// main.cc
 //	Bootstrap code to initialize the operating system kernel.
 //
 //	Allows direct calls into internal operating system functions,
@@ -31,7 +31,7 @@
 //    -p prints a Nachos file to stdout
 //    -r removes a Nachos file from the file system
 //    -l lists the contents of the Nachos directory
-//    -D prints the contents of the entire file system 
+//    -D prints the contents of the entire file system
 //    -t tests the performance of the Nachos file system
 //
 //  NETWORK
@@ -43,7 +43,7 @@
 //  Some of the flags are interpreted here; some in system.cc.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #define MAIN
@@ -59,21 +59,23 @@ extern int testnum;
 
 // External functions used by this file
 
+extern void SemaphorePing(void);
 extern void ThreadTest(int), Copy(char *unixFile, char *nachosFile);
 extern void Print(char *file), PerformanceTest(void);
 extern void StartProcess(char *file), ConsoleTest(char *in, char *out);
 extern void MailTest(int networkID);
+extern void Ping(void);
 
 //----------------------------------------------------------------------
 // main
-// 	Bootstrap the operating system kernel.  
-//	
+// 	Bootstrap the operating system kernel.
+//
 //	Check command line arguments
 //	Initialize data structures
 //	(optionally) Call test procedure
 //
 //	"argc" is the number of command line arguments (including the name
-//		of the command) -- ex: "nachos -d +" -> argc = 3 
+//		of the command) -- ex: "nachos -d +" -> argc = 3
 //	"argv" is an array of strings, one for each command line argument
 //		ex: "nachos -d +" -> argv = {"nachos", "-d", "+"}
 //----------------------------------------------------------------------
@@ -81,12 +83,12 @@ extern void MailTest(int networkID);
 int
 main(int argc, char **argv)
 {
-    int argCount;			// the number of arguments 
+    int argCount;			// the number of arguments
 					// for a particular command
 
     DEBUG('t', "Entering main");
     (void) Initialize(argc, argv);
-    
+
 #ifdef THREADS
     for (argc--, argv++; argc > 0; argc -= argCount, argv += argCount) {
       argCount = 1;
@@ -100,8 +102,19 @@ main(int argc, char **argv)
         break;
       }
     }
-	
-    ThreadTest(2);
+
+    #ifndef HW1_CONDITION
+		ThreadTest(3);
+	#endif
+
+	#if defined(HW1_CONDITION)
+		Ping();
+	#endif
+
+	#if defined(HW1_ELEVATOR)
+    	Elevator(4);
+    	ArrivingGoingFromTo(1,4);
+	#endif
 #endif
 
     for (argc--, argv++; argc > 0; argc -= argCount, argv += argCount) {
@@ -121,8 +134,8 @@ main(int argc, char **argv)
 	        ConsoleTest(*(argv + 1), *(argv + 2));
 	        argCount = 3;
 	    }
-	    interrupt->Halt();		// once we start the console, then 
-					// Nachos will loop forever waiting 
+	    interrupt->Halt();		// once we start the console, then
+					// Nachos will loop forever waiting
 					// for console input
 	}
 #endif // USER_PROGRAM
@@ -151,7 +164,7 @@ main(int argc, char **argv)
         if (!strcmp(*argv, "-o")) {
 	    ASSERT(argc > 1);
             Delay(2); 				// delay for 2 seconds
-						// to give the user time to 
+						// to give the user time to
 						// start up another nachos
             MailTest(atoi(*(argv + 1)));
             argCount = 2;
@@ -159,7 +172,7 @@ main(int argc, char **argv)
 #endif // NETWORK
     }
 
-    currentThread->Finish();	// NOTE: if the procedure "main" 
+    currentThread->Finish();	// NOTE: if the procedure "main"
 				// returns, then the program "nachos"
 				// will exit (as any other normal program
 				// would).  But there may be other
